72d41f09058a9a24f4fbffd9361621bb2c1c7593
HannaKorol <162054571+HannaKorol@users.noreply.github.com>
2024-07-23 20:41:25 +0000

    Deploying to gh-pages from @ HannaKorol/goit-js-hw-09@5a3a162050bc7365a414e41179293a9ddc3f0e03 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */console.log("Gallery");
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{S as o}from"./assets/vendor-10cb7c31.js";new o(".gallery a",{});console.log(o);
 //# sourceMappingURL=commonHelpers.js.map
