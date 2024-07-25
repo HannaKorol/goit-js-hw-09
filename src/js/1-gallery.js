7c0b3562e1e0f8990b5f4de84ddba4bf424c0996
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-07-25 18:21:29 +0200

    gallery

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -3,11 +3,6 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const lightbox = new SimpleLightbox('.gallery a', {
-    /* options*/
-});
-console.log(SimpleLightbox);
-
 
 const images = [
     {
